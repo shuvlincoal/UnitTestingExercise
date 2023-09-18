f56c0330dfa8c69211ba454afdd906a2abef2de1
mangosax <mcguerty@att.net>
2023-09-18 14:17:12 -0500

    3rd push

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -9,12 +9,15 @@ namespace UnitTestingExercise.Tests
     {
         [Theory]
         [InlineData(2, 3, 5, 10)] //Add test data <-------
+        [InlineData(6, 6, 8, 20)]
+        [InlineData(10, 10, 20, 40)]
         public void AddTest(int num1, int num2, int num3, int expected)
         {
             //Start Step 3 here:
 
             //Arrange
             // create a Calculator object
+            // Creating an ojbect == create instance
             //Calculator instance = new Calculator();
             var calc = new Calculator();
 
@@ -30,8 +33,9 @@ namespace UnitTestingExercise.Tests
         }
 
         [Theory]
-        [InlineData(1, 1, 0)]//Add test data <-------
+        [InlineData(1, 1, 0)]//Sub test data <-------
         [InlineData(1, 4, 3)]
+        [InlineData(7, 21, 14)]
         public void SubtractTest(int minuend, int subtrahend, int expected)
         {
             //Start Step 5 here:
@@ -47,7 +51,9 @@ namespace UnitTestingExercise.Tests
         }
 
         [Theory]
-        [InlineData(4, 2, 8)]//Add test data <-------
+        [InlineData(4, 2, 8)]//Multiply test data <-------
+        [InlineData(13, 2, 26)]
+        [InlineData(12, 12, 144)]
         public void MultiplyTest(int num1, int num2, int expected)
         {
             //Start Step 7 here:
@@ -64,7 +70,10 @@ namespace UnitTestingExercise.Tests
         }
 
         [Theory]
-        [InlineData(16, 4, 4)]//Add test data <-------
+        [InlineData(16, 4, 4)]//Div test data <-------
+        [InlineData(18, 9, 2)]
+        [InlineData(15, 3, 5)]
+        [InlineData(48, 2, 24)]
         public void DivideTest(int num1, int num2, int expected)
         {
             //Arrange
